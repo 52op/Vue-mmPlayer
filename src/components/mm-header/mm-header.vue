a3395ed5580f2fa06403d10217348c78bf5b383d
52op <121745566+52op@users.noreply.github.com>
2023-09-03 18:22:22 +0800

    Update mm-header.vue

--- src/components/mm-header/mm-header.vue
+++ src/components/mm-header/mm-header.vue
@@ -2,8 +2,8 @@
   <!--头部-->
   <header class="mm-header">
     <h1 class="header">
-      <a href="https://github.com/maomao1996/Vue-mmPlayer" target="_blank">
-        mmPlayer 在线音乐播放器
+      <a href="https://www.sztcrs.com" target="_blank">
+        天成锐视在线音乐播放器
       </a>
       <img
         v-if="visitorBadge"
